4e6f6952d73631e53ae3d5df03f5a3c0f4ec8610
alex jussiani junior <alexjunior_jussiani@hotmail.com>
2019-09-12 22:25:52 -0300

    banco

--- ERP/Entidades/ItensLanches.cs
+++ ERP/Entidades/ItensLanches.cs
@@ -8,6 +8,8 @@ namespace ERP.Dominio.Entidades
     {
         public int id { get; set; }
         public int lancheId { get; set; }
+        public virtual Lanches lanches { get; set; }
+        public virtual Ingredientes Ingredientes { get; set; }
         public int IngredienteId { get; set; }
 
     }
